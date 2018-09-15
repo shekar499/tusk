111bc5d0bbae4a991aaf813a6c35919616375851
shekar <shekar@DESKTOP-HTMPCV3>
2018-09-15 21:53:32 +0530

    removed setter methods

--- GitProject/src/comparator/Employee.java
+++ GitProject/src/comparator/Employee.java
@@ -6,15 +6,11 @@ public class Employee {
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + "]";
